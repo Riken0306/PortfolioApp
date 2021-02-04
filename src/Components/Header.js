f77069e5b190df4c65f2692eee127e9d56752d3b
Riken0306 <riken0306@gmail.com>
2021-02-04 08:49:54 +0530

    First Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,111 @@
+import React from "react";
+// import TypeWriter from "react-typewriter";
+// import Typewriter from 'typewriter-effect';
+import Typical from "react-typical";
+
+const Header = ({ data }) => {
+  if (data) {
+    var name = data.name;
+    var occupation = data.occupation;
+    var description = data.description;
+    var city = data.address.city;
+    var state = data.address.state;
+    var networks = data.social.map(function (network) {
+      return (
+        <li key={network.name}>
+          <a href={network.url}>
+            <i className={network.className}></i>
+          </a>
+        </li>
+      );
+    });
+  }
+
+  return (
+    <header id="home">
+      <nav id="nav-wrap">
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+          Show navigation
+        </a>
+        <a className="mobile-btn" href="#home" title="Hide navigation">
+          Hide navigation
+        </a>
+
+        <ul id="nav" className="nav">
+          <li className="current">
+            <a className="smoothscroll" href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#resume">
+              Resume
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#portfolio">
+              Works
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#testimonials">
+              Testimonials
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="row banner">
+        <div className="banner-text">
+          <h1 className="responsive-headline">
+            {/* <TypeWriter typing={0.5}>{name ? `I'm ${name}.` : null}</TypeWriter> */}
+            {/* <h1>{name ? `I'm ${name}.` : null}</h1> */}
+            <h1>I'm {`${name}`}</h1>
+            <p>
+              A{" "}
+              <Typical
+                loop={Infinity}
+                steps={[
+                  "Engineer",
+                  2000,
+                  "Developer",
+                  2000,
+                  "Blogger",
+                  2000,
+                  "Open-Sourcer",
+                ]}
+              />
+            </p>
+          </h1>
+          <h3>
+            Based in {state}.
+            <br />
+            <span>{occupation}</span>.
+            <br />
+            {description}.
+          </h3>
+          <hr />
+          <ul className="social">{networks}</ul>
+        </div>
+      </div>
+
+      <p className="scrolldown">
+        <a className="smoothscroll" href="#about">
+          <i className="icon-down-circle"></i>
+        </a>
+      </p>
+    </header>
+  );
+};
+
+export default Header;
